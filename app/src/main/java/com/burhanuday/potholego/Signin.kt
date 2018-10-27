a138fc226270091a617ea8d0520ed7cdf48d46db
Burhanuday <udaipurwalaburhanuddin@gmail.com>
2018-10-27 21:58:45 +0530

    added signin activity

---
+++ app/src/main/java/com/burhanuday/potholego/Signin.kt
@@ -0,0 +1,12 @@
+package com.burhanuday.potholego
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class Signin : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signin)
+    }
+}
