a2cf2fdb266cc3b12bc1c87baa2d5c9b64878f01
Idrees Dargahwala <idrees@theiyd.com>
2018-11-21 18:35:27 +0530

    log the bearer

--- helpers/verifyToken.js
+++ helpers/verifyToken.js
@@ -2,6 +2,7 @@ function verifyToken(req, res, next) {
   const bearerHeader = req.headers["authorization"];
   if (typeof bearerHeader !== "undefined") {
     const bearer = bearerHeader.split(" ");
+    console.log(bearer)
     const bearerToken = bearer[1];
     console.log(bearerToken);
     req.token = bearerToken;
