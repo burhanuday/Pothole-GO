09d9c7d7ef65c03e3a9f595893bfb86bf529c829
Utkarsh Tiwari <utkarsh.tiwari@somaiya.edu>
2018-12-08 18:12:13 +0530

    Add files via upload

---
+++ Pothole-Go-Python/test_sizeDetection/main_run.py
@@ -0,0 +1,4 @@
+from test_sizeDetection.test3_objectParameters import final
+
+if __name__ == '__main__':
+    final()
--- Pothole-Go-Python/test_sizeDetection/test3_objectParameters.py
+++ Pothole-Go-Python/test_sizeDetection/test3_objectParameters.py
@@ -1,15 +1,23 @@
-from potholeHeight_test1.test_sizeDetection.pydonesia import ComputerVision
+from test_sizeDetection.measurement_pydo import ComputerVision
 import os
+import sys, json, numpy as np
 
-cv = ComputerVision()
+class final:
 
-cwd = os.getcwd()
-file_all = os.listdir(cwd)
+    cv = ComputerVision()
 
-images = []
-for f in file_all:
-    if f.lower().endswith('jpg'): images.append(f)  # checks for all files with the given extension
+    cwd = os.getcwd()
+    file_all = os.listdir(cwd)
 
-for i in images:
-    image = i
-    cv.measure_object_dimension(image, coin_diameter=24, unit='mm')  # mm measure
+    def read_line(self):
+        lines = sys.stdin.readlines()
+        return json.loads(lines[0])
+
+    images = read_line()
+
+    for f in file_all:
+        if f.lower().endswith('jpg'): images.append(f)  # checks for all files with the given extension
+
+    for i in images:
+        image = i
+        cv.measure_object_dimension(image, coin_diameter=24, unit='mm')  # mm measure
