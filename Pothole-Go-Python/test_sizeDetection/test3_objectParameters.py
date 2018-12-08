ae1719d2e1c7b52e7911881fe1347feab1a61cce
Utkarsh Tiwari <utkarsh.tiwari@somaiya.edu>
2018-12-08 18:44:12 +0530

    Add files via upload

--- Pothole-Go-Python/test_sizeDetection/test3_objectParameters.py
+++ Pothole-Go-Python/test_sizeDetection/test3_objectParameters.py
@@ -1,6 +1,5 @@
 from test_sizeDetection.measurement_pydo import ComputerVision
 import os
-import sys, json, numpy as np
 
 class final:
 
@@ -9,15 +8,12 @@ class final:
     cwd = os.getcwd()
     file_all = os.listdir(cwd)
 
-    def read_line(self):
-        lines = sys.stdin.readlines()
-        return json.loads(lines[0])
-
-    images = read_line()
-
+    images = []
+'''
     for f in file_all:
         if f.lower().endswith('jpg'): images.append(f)  # checks for all files with the given extension
 
     for i in images:
         image = i
         cv.measure_object_dimension(image, coin_diameter=24, unit='mm')  # mm measure
+'''
